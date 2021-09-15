2982e056dca8ef9e6337f2a655abc251f1f54b0d
Cubxity <contact@cubxity.dev>
2021-09-15 17:38:19 +0200

    style: improved formatting

--- packages/auth-mojang/jest.config.js
+++ packages/auth-mojang/jest.config.js
@@ -21,13 +21,13 @@ module.exports = {
   preset: "../../jest.preset.js",
   globals: {
     "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.spec.json",
-    },
+      tsconfig: "<rootDir>/tsconfig.spec.json"
+    }
   },
   testEnvironment: "node",
   transform: {
-    "^.+\\.[tj]sx?$": "ts-jest",
+    "^.+\\.[tj]sx?$": "ts-jest"
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../coverage/packages/auth-mojang",
+  coverageDirectory: "../../coverage/packages/auth-mojang"
 };
